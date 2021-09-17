da413a50492b3a53c594c1680dd0e1503665f2cb
Prince Raj <63599436+StarkPrince@users.noreply.github.com>
2021-09-17 08:53:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -59,5 +59,3 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
     .catch((error) => console.log(`${error} did not connect`));
-
-    // CONNECTION_URL=mongodb+srv://StarkPrince:M14t85u2m1nt93S@cluster0.bmaqq.mongodb.net/lapify?retryWrites=true&w=majority
